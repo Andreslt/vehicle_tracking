d63f33953a1f27ee82a83db7ccc8262e882b50f6
Andres Lastra <Andres@MacBook-Pro-de-Andres-4.local>
2018-06-05 16:07:05 -0500

    adding 'Retransmitido' mark to snippet

--- src/components/dump/Snippet/index.js
+++ src/components/dump/Snippet/index.js
@@ -4,12 +4,12 @@ class Snippet extends Component {
   render() {
     return (
       <div>
-        <b>Coordenadas</b><br />
+        <b>Coordenadas</b>{!!this.props.point.bulk && <b> (Retransmitido)</b>}<br />
         Latitud: {this.props.point.lat}<br />
         Longitud: {this.props.point.lng}<br />
         Altitud: {this.props.point.altitude}<br />
         Velocidad: {this.props.point.speed}<br />
-        timestamp: {this.props.point.sent_tsmp}
+        timestamp: {this.props.point.sent_tsmp}     
       </div>
     )
   }
